f62823a9b8b4b520007f7613f7ab052be1bdc10b
Adam Marsala <adammarsala@hotmail.com>
2022-06-07 10:35:05 -0400

    Add style to Home buttons and global font

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 export default function Home() {
   const reg = <sup>&reg;</sup>; // ®
 
   return (
     <div className="home">
-      <h1>INVEGA SUSTENNA{reg} Dosing Conversion Tool </h1>
+      <h1>INVEGA SUSTENNA{reg}<br/> Dosing Conversion Tool </h1>
       <div className="grid-2x2">
-        <p>Switch your patients from oral antipsychotics to INVEGA SUSTENNA{reg}</p>
-        <p>Switch your patients from INVEGA SUSTENNA{reg} to INVEGA TRINZA{reg}</p>
-        <Link to="/dosing-tool-invega-sustenna">Oral Antipsychotics to INVEGA SUSTENNA® Dosing Conversion Guide</Link>
-        <Link to="/dosing-tool-invega-trinza">INVEGA SUSTENNA® to INVEGA TRINZA® Dosing Conversion Guide</Link>
+        <p>Switch your patients from oral antipsychotics<br/> to INVEGA SUSTENNA{reg}</p>
+        <p>Switch your patients from INVEGA SUSTENNA{reg}<br/> to INVEGA TRINZA{reg}</p>
+        <Link className="bg-yellow btn" to="/dosing-tool-invega-sustenna">Oral Antipsychotics to INVEGA&nbsp;SUSTENNA®<br/> Dosing Conversion Guide</Link>
+        <Link className="bg-blue btn" to="/dosing-tool-invega-trinza">INVEGA SUSTENNA® to INVEGA&nbsp;TRINZA®<br/> Dosing Conversion Guide</Link>
       </div>
     </div>
   )

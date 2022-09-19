4e2558ede948ccd40efff0a16ef2b805b243b3b5
Adam Marsala <adammarsala@hotmail.com>
2022-09-19 10:18:18 -0400

    Commit to remote repo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,9 @@ import home from "../assets/home.png";
 import topLine from "../assets/topLine.png";
 // Modal
 import Modal from "./materials/Modal";
+// logos
+import sustenna from "../assets/sustenna.png";
+import trinza from "../assets/trinza.png";
 
 
 export default function Nav({ modal, toggle, closeModal, }) {
@@ -26,6 +29,10 @@ export default function Nav({ modal, toggle, closeModal, }) {
       </div>
       <div className={modal ? "blur botrow" : "botrow"}>
         <Link to="/dosing-tool"><img className="home-icon" src={home} alt="Home" aria-label="Home" role="button" /></Link>
+        <div className='chevron-logos'>
+          <img className="sustenna-logo" src={sustenna} alt="ONCE-MONTHLY INVEGA SUSTENNA® paliperidone palmitate PROLONGED-RELEASE INJECTABLE SUSPENSION" />
+          <img className="trinza-logo" src={trinza} alt="PRINVEGA TRINZA® paliperidone palmitate prolonged-release injectable suspension 175 mg, 263 mg, 350 mg, 525 mg" />
+        </div>        
         <button className="modal-button si-button" onClick={() => toggle()}>
           Safety Information
         </button>

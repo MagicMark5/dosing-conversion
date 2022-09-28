88296131061f823f9b3ed00cdb560c3c38b832fa
Adam Marsala <adammarsala@hotmail.com>
2022-09-28 17:11:41 -0400

    Footer desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,46 @@
 import '../styles/Footer.scss';
+// images
+import divider from "../assets/divider.png";
+import janssen from "../assets/janssen.png";
+import imc from "../assets/imc.png";
+import paab from "../assets/paab.png";
 
 export default function Footer({modal}) {
 
-  // Balance stays visible on every page of the app
+  const bar = <span>&nbsp;|&nbsp;</span>;
 
   return (
-    <>
-      <footer className={modal ? "blur footer flex-col" : "footer flex-col"}>
+    <footer className={modal ? "blur page-footer" : "page-footer"}>
+      <img className="footer-divider" src={divider} alt="footer divider" />
+      <div className='legal-logos-container'>
         <p className="legal">
-          © 2022 Janssen Inc.<br/><br/>
-          All trademarks used under license.<br/>
-          Janssen Inc.<br/><br/>
-          19 Green Belt Drive <br/>
-          Toronto, Ontario  M3C 1L9<br/>
-          <a href="https://www.janssen.com/canada" rel="noreferrer" target="_blank">www.janssen.com/canada</a><br/>
+          <strong>References:</strong> 1. INVEGA SUSTENNA® Product Monograph. Janssen Inc. December 17, 2020. 2. INVEGA TRINZA® Product Monograph. Janssen Inc. December 17, 2020.<br/><br/>
+          © 2022 Janssen Inc. {bar} All trademarks used under license.<br/><br/>
+          Privacy Policy {bar} Terms of Use<br/><br/>
+          Janssen Inc.<br/>
+          19 Green Belt Drive {bar} Toronto, Ontario  M3C 1L9<br/>
+          <a href="https://www.janssen.com/canada" rel="noreferrer" target="_blank">www.janssen.com/canada</a>
         </p>
-        <div className="logos flex-col">
+        <div className="logos">
           <img alt="Member of Innovative Medicines Canada logo" 
             height="auto" 
-            src="https://email-builder-c70fb.appspot.com.storage.googleapis.com/orgs/maven/projects/adam's-starter/images/imc-logo-white-en-xFyAEg-.png" 
-            title="Member of Innovative Medicines Canada logo" 
-            width="150"
+            src={imc}
+            title="Member of Innovative Medicines Canada logo"
           />
           <img alt="PAAB" 
             height="auto" 
-            src="https://email-builder-c70fb.appspot.com.storage.googleapis.com/orgs/maven/projects/adam's-starter/images/paab-logo-en-bZ1Fyg-.png" 
-            width="87"
+            src={paab}
           />
           <img alt="Janssen Pharmaceutical Companies of Johnson &#x26; Johnson" 
             height="auto" 
-            src="https://email-builder-c70fb.appspot.com.storage.googleapis.com/orgs/maven/projects/adam's-starter/images/janssen-logo-CNWFqg-.png" 
+            src={janssen} 
             title="Janssen Pharmaceutical Companies of Johnson &#x26; Johnson" 
-            width="180"
           />          
         </div>
-      </footer>
-    </>
+      </div>
+      <div className='legal project-code'>
+        CP-312685E
+      </div>
+    </footer>
   )
 }

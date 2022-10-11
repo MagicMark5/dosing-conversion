04b66a60f484e20f036de5264f373fa6f436c680
Adam Marsala <adammarsala@hotmail.com>
2022-10-10 20:36:22 -0400

    Make mobile chevron responsive for mobile

--- src/components/materials/Chevron.js
+++ src/components/materials/Chevron.js
@@ -2,11 +2,13 @@ import '../../styles/Chevron.scss';
 
 export default function Chevron({ heading }) {
   return (
-    <div className='chevron-wrapper'>
-      <div className="chevron-container">
-        <div className="heading">{heading}</div>
+    <div className="flex-center">
+      <div className='chevron-wrapper'>
+        <div className="chevron-container">
+          <div className="heading">{heading}</div>
+        </div>
+        <div className="chevron"></div>
       </div>
-      <div className="chevron"></div>
     </div>
   )
 }

cde3e4b21ddad3a95137f212927a4814db82961f
Adam Marsala <adammarsala@hotmail.com>
2022-10-17 10:40:58 -0400

    Revert back to staging url with github pages

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import App from './components/App';
 // react-router-dom node package
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const baseURL = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : '/';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={baseURL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>

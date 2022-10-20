d15dfc964bce4f6e72f98d74f04bfc3fc8d9c6d5
Adam Marsala <adammarsala@hotmail.com>
2022-10-20 13:12:21 -0400

    Update footer for french translations and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
 import '../styles/Footer.scss';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import TranslationContext from '../context/TranslationContext';
+import { useTranslation } from "react-i18next";
 // images
 import divider from "../assets/divider.png";
 import janssen from "../assets/janssen.png";
 import imc from "../assets/imc.png";
+import imc_fr from "../assets/imc_fr.png";
 import paab from "../assets/paab.png";
+import paab_fr from "../assets/paab_fr.png";
 
 export default function Footer({modal}) {
-
+  const { t, i18n } = useTranslation();
+  const lang = i18n.resolvedLanguage; // current language
+  const { enFr, d_trinza, d_sustenna, sustenna, trinza } = useContext(TranslationContext); // brand names with non-breaking spaces
   const bar = <span>&nbsp;|&nbsp;</span>;
 
   return (
@@ -15,26 +22,34 @@ export default function Footer({modal}) {
       <img className="footer-divider" src={divider} alt="" />
       <div className='legal-logos-container'>
         <p className="legal">
-          <strong>References:</strong> 1.&nbsp;INVEGA SUSTENNA® Product Monograph. Janssen Inc. December 17, 2020. 2.&nbsp;INVEGA TRINZA® Product Monograph. Janssen Inc. December 17, 2020.<br/><br/>
-          © 2022 Janssen Inc. {bar} All trademarks used under license.<br/><br/>
-          <a href="https://www.janssen.com/canada/privacy" target="_blank" rel="noreferrer">Privacy Policy</a> {bar} 
-          <Link to="/terms-of-use">Terms of Use</Link><br/><br/>
+          <strong>{t('footer.refs')}</strong>
+          1.&nbsp;{enFr(
+            <>{sustenna} {t('footer.pm')}</>, // en
+            <> {t('footer.pm_a')} {d_sustenna}. {t('footer.pm_b')}</> // fr
+          )}
+          2.&nbsp;{enFr(
+            <>{trinza} {t('footer.pm')}</>, // en
+            <> {t('footer.pm_a')} {d_trinza}. {t('footer.pm_b')}</> // fr
+          )}
+          <br/><br/>
+          © 2022 Janssen Inc. {bar} {t('footer.tm')}<br/><br/>
+          <a href={t('footer.pp_url')} target="_blank" rel="noreferrer">{t('footer.pp')}</a> {bar} <Link to="/terms-of-use">{t('footer.terms')}</Link><br/><br/>
           Janssen Inc.<br/>
-          19 Green Belt Drive {bar} Toronto,&nbsp;Ontario  M3C&nbsp;1L9<br/>
-          <a href="https://www.janssen.com/canada" rel="noreferrer" target="_blank">www.janssen.com/canada</a>
+          19 Green Belt Drive {bar} <span className='nowrap'>{t('footer.city')}</span>  M3C&nbsp;1L9<br/>
+          <a href={t('footer.company_url')} rel="noreferrer" target="_blank">www.janssen.com/canada{lang === 'fr' && '/fr'}</a>
           <span className='legal mobile project-code'>
             <br/><br/>
-            CP-312685E
-          </span>          
+            CP-312685{enFr("E", "F")}
+          </span>
         </p>
         <div className="logos">
-          <img alt="Member of Innovative Medicines Canada logo"
+          <img alt={t('footer.imc')}
             className="imc-logo"
-            src={imc}
+            src={enFr(imc, imc_fr)}
           />
-          <img alt="PAAB"
+          <img alt={t('footer.paab')}
             className="paab-logo"
-            src={paab}
+            src={enFr(paab, paab_fr)}
           />
           <img alt="Janssen Pharmaceutical Companies of Johnson &#x26; Johnson"
             className="janssen-logo"
@@ -43,7 +58,7 @@ export default function Footer({modal}) {
         </div>
       </div>
       <div className='legal desktop project-code'>
-        CP-312685E
+        CP-312685{enFr("E", "F")}
       </div>
     </footer>
   )
--- src/context/TranslationProvider.js
+++ src/context/TranslationProvider.js
@@ -2,15 +2,18 @@ import TranslationContext from "./TranslationContext";
 import { useTranslation } from "react-i18next";
 
 export default function TranslationProvider({ children }) {
-  const { t } = useTranslation();
-  
+  const { t, i18n } = useTranslation();
+  const lang = i18n.resolvedLanguage; // current language
   const sustenna = <span className="nowrap">{t('sustenna')}</span>;
   const trinza = <span className="nowrap">{t('trinza')}</span>;
   const d_sustenna = <span className="nowrap">{t('d_apos_sustenna')}</span>;
   const d_trinza = <span className="nowrap">{t('d_apos_trinza')}</span>;
   const qu_sustenna = <span className="nowrap">{t('qu_apos_sustenna')}</span>;
 
+  const enFr = (en, fr) => lang === "fr" ? fr : en;
+
   const value = {
+    enFr,
     sustenna,
     trinza,
     d_sustenna,

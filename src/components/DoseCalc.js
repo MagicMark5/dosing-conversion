8404245f245beae0b800d74068bde91929741404
Adam Marsala <adammarsala@hotmail.com>
2023-01-11 12:28:47 -0500

    Add tab Index to dose calc tab buttons and ensure reading order is correct

--- src/components/DoseCalc.js
+++ src/components/DoseCalc.js
@@ -8,11 +8,12 @@ export default function DoseCalc({ inputLabel, outputLabel, doseIO, bgOutput, co
 
   const inputs = Object.keys(doseIO);
   // ids of tab buttons are zero indexed as i.e. tab-0, tab-1, tab-2, etc.
-  const inputIds = inputs.map((input, index) => `tab-${index}`);
+  const inputIds = inputs.map((_, index) => `tab-${index}`);
   const output = Object.values(doseIO);
 
   function doseChange(e) {
     const index = Number(e.target.id.split("-")[1]);
+    // change selected dose state
     setDose(index);
   };
 
@@ -23,7 +24,8 @@ export default function DoseCalc({ inputLabel, outputLabel, doseIO, bgOutput, co
   useEffect(() => {
 
     function handleKeyDown(event) {
-      // if the focus is not on the tabs, or if we use Tab to change focus, then return
+
+      // if the focus is not on the tabs or tabpanel, or if we use Tab to change focus, then return
       if (!inputIds.includes(document.activeElement.id) || event.key === "Tab") {
         return;
       }
@@ -48,10 +50,25 @@ export default function DoseCalc({ inputLabel, outputLabel, doseIO, bgOutput, co
             document.getElementById(`tab-${currentFocusElementIndex + 1}`).focus();
           }
           break;
+        case ("ArrowLeft"):
+          if (currentFocusElementIndex === 0) {
+            document.getElementById(inputIds[inputIds.length - 1]).focus();
+          } else {
+            document.getElementById(`tab-${currentFocusElementIndex - 1}`).focus();
+          };
+          break;
+        case ("ArrowRight"):
+          if (currentFocusElementIndex === inputIds.length - 1) {
+            document.getElementById(inputIds[0]).focus();
+          } else {
+            document.getElementById(`tab-${currentFocusElementIndex + 1}`).focus();
+          }
+          break;
         case ("Enter"):
+          // Click the focussed tab
           document.getElementById(`tab-${currentFocusElementIndex}`).click();
           break;
-        default: 
+        default:
           return;
       }
     };
@@ -91,6 +108,7 @@ export default function DoseCalc({ inputLabel, outputLabel, doseIO, bgOutput, co
                   type="button"
                   aria-selected={dose === index}
                   aria-controls={`tabpanel-${index}`}
+                  tabIndex={dose !== index ? -1 : 0}
                   id={`tab-${index}`}
                   onClick={doseChange}>
                     {/* Ensure screen readers say the "column" heading but is not visible */}
@@ -103,18 +121,21 @@ export default function DoseCalc({ inputLabel, outputLabel, doseIO, bgOutput, co
             )
           })}
         </div>
-        {inputs.map((input, index) => {
+        {inputs.map((_, index) => {
+            const typeOfOutput = typeof output[index].value;
+            const screenReaderOutput = `${ariaLabelOutput} ${typeOfOutput === "object" ? "25 to 50 mg" : output[dose].value}`;
             return (
               <div
                 key={`output dose result: ${index}`}
                 className={dose === index ? `output-cell ${bgOutput} ${colorOutput} ${rightColWidth}` : `is-hidden`} 
                 id={`tabpanel-${index}`} 
-                role="tabpanel" 
+                tabIndex={0}
+                role="tabpanel"
                 aria-labelledby={`tab-${index}`}>                
                 {/* Ensure screen readers say the "column" heading but is not visible */}
-                <span className="sr-only">{ariaLabelOutput}</span>
+                <span className="sr-only">{screenReaderOutput}</span>
                 {/* Visible number of mg output value */}
-                <span>{output[dose].value}</span>      
+                <span aria-hidden="true">{output[dose].value}</span>
               </div>
             )
           })}
